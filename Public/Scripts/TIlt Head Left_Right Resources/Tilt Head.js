c682a83b92cac9b5657b95dcaa83c40761e7c369
Caleb Geiger <caleblgeiger@gmail.com>
2024-02-11 08:38:39 -0500

    Move Quiz Logic into Head Tilt Script.
    Add basic round start/end logic based on lives and add score.

--- Public/Scripts/TIlt Head Left_Right Resources/Tilt Head.js
+++ Public/Scripts/TIlt Head Left_Right Resources/Tilt Head.js
@@ -5,37 +5,44 @@
 
 //@input float angle {"widget" : "slider", "min" : 0, "max" : 90, "step" : 0.1}
 /** @type {number} */
-var angle = script.angle;
+let angle = script.angle;
 
 //@input int faceIndex
 /** @type {number} */
-var faceIndex = script.faceIndex;
+let faceIndex = script.faceIndex;
 
-//@ui {"widget": "separator"}
-//@input bool useBehavior
-//@input string onLeftTrigger = "ON_LEFT" {"showIf" : "useBehavior"}
-//@input string onRightTrigger = "ON_RIGHT"  {"showIf" : "useBehavior"}
-//@input string onResetTrigger = "ON_RESET"  {"showIf" : "useBehavior"}
-//@ui {"widget":"separator"}
-//@input bool callApiFunc
-//@input Component.ScriptComponent scriptWithApi  {"showIf" : "callApiFunc"}
-//@input string onLeftFunction = "onLeft" {"showIf" : "callApiFunc"}
-//@input string onRightFunction = "onRight"  {"showIf" : "callApiFunc"}
-//@input string onResetFunction = "onReset"  {"showIf" : "callApiFunc"}
+
+//@input Component.MaterialMeshVisual indicator
+
+//@input Asset.Material matGreen
+//@input Asset.Material matGrey
+//@input Asset.Material matRed
+
+//@input Component.Text leftChoice
+//@input Component.Text rightChoice
+
+//@input Component.Text attemptText
+//@input Component.Text attemptValue
+
+//@input Component.Text scoreText
+//@input Component.Text scoreValue
 
 const eps = 0.1;
-var State = { "NONE": 0, "LEFT": 1, "RIGHT": 2 };
-var currentState = State.NONE;
-var x;
-var threshold = Math.abs(Math.sin(angle / 180 * Math.PI))
+let State = { "NONE": 0, "LEFT": 1, "RIGHT": 2 };
+let currentState = State.NONE;
+let x;
+let threshold = Math.abs(Math.sin(angle / 180 * Math.PI))
 
-var so = script.getSceneObject();
-var head = global.scene.createSceneObject("Head Binding");
+let so = script.getSceneObject();
+let head = global.scene.createSceneObject("Head Binding");
 head.setParent(so);
-var headTransform = head.getTransform();
-var headComponent = head.createComponent("Component.Head");
+let headTransform = head.getTransform();
+let headComponent = head.createComponent("Component.Head");
 headComponent.faceIndex = faceIndex;
 
+script.createEvent("OnStartEvent").bind(function () {
+    resetState();
+});
 script.createEvent("UpdateEvent").bind(onUpdate);
 
 function onUpdate() {
@@ -65,29 +72,94 @@ function onUpdate() {
     }
 }
 
+function bottomTextVisible(b) {
+    script.attemptText.enaled = b;
+    script.attemptValue.enaled = b;
+    script.scoreText.enaled = b;
+    script.scoreValue.enaled = b;
+}
+
+const DEFAULT_LIVES = 2;
+let quizState = {
+    firstTime: true,
+    end: false,
+};
+
+function resetState() {
+    let prevScore = 0;
+    if (!(quizState === undefined) && !(quizState.correctAnswers === undefined)) {
+        prevScore = quizState.correctAnswers;
+    }
+    if (quizState.firstTime) {
+        quizState = {
+            leftChoice: "Tilt Head",
+            rightChoice: "To Start Quiz!",
+            
+            firstTime: false,
+            attemptsRemaining: DEFAULT_LIVES,
+            correctAnswers: 0,
+            questionNumber: 0,
+            end: quizState.end,
+        };        
+    } else {
+        quizState = {
+            leftChoice: "Try Again?",
+            rightChoice: "SCORE: " + prevScore.toString(),
+            
+            firstTime: false,
+            attemptsRemaining: DEFAULT_LIVES,
+            correctAnswers: 0,
+            questionNumber: 0,
+            end: quizState.end,
+        };
+    }
+    bottomTextVisible(false);
+    onReset();
+}
+
+function checkEnd() {
+    if (quizState.attemptsRemaining == 0) {
+        quizState.end = true;
+        resetState();
+        script.indicator.mainMaterial = script.matRed;
+    }
+}
+
+function printCurrentState()  {
+    script.attemptValue.text = quizState.attemptsRemaining.toString();
+    script.scoreValue.text = quizState.correctAnswers.toString();
+    
+    script.leftChoice.text = quizState.leftChoice;
+    script.rightChoice.text = quizState.rightChoice;
+}
+
 function onLeft() {
-    if (script.callApiFunc && script.scriptWithApi && script.onLeftFunction && script.scriptWithApi[script.onLeftFunction]) {
-        script.scriptWithApi[script.onLeftFunction]();
+    if (quizState.end) {
+        quizState.end = false;
+        resetState();
     }
-    if (script.useBehavior && global.behaviorSystem && script.onLeftTrigger) {
-        global.behaviorSystem.sendCustomTrigger(script.onLeftTrigger);
-    }
+    script.indicator.mainMaterial = script.matGreen;
+
+    quizState.correctAnswers += 1;
+    script.leftChoice.text = "Left";
+    checkEnd();
 }
 
 function onRight() {
-    if (script.callApiFunc && script.scriptWithApi && script.onRightFunction && script.scriptWithApi[script.onRightFunction]) {
-        script.scriptWithApi[script.onRightFunction]();
+    if (quizState.end) {
+        return;
     }
-    if (script.useBehavior &&global.behaviorSystem && script.onRightTrigger) {
-        global.behaviorSystem.sendCustomTrigger(script.onRightTrigger);
-    }
+    script.indicator.mainMaterial = script.matRed;
+
+    quizState.attemptsRemaining -= 1;
+    script.rightChoice.text = "Right";
+    checkEnd();
 }
 
 function onReset() {
-    if (script.callApiFunc && script.scriptWithApi && script.onResetFunction && script.scriptWithApi[script.onResetFunction]) {
-        script.scriptWithApi[script.onResetFunction]();
-    }
-    if (script.useBehavior &&global.behaviorSystem && script.onResetTrigger) {
-        global.behaviorSystem.sendCustomTrigger(script.onResetTrigger);
-    }
+    script.indicator.mainMaterial = script.matGrey;
+
+    script.leftChoice.text = "Default";
+    script.rightChoice.text = "Default";
+    printCurrentState();
 }
